ed65958d55d13b4ec5cfbf18a4b9532b32dbdc67
countxyz <skeptoid@gmail.com>
2013-09-08 18:41:17 -0400

    Add get_csv_data method

--- lib/nyccb_index_generator/json_assembler.rb
+++ lib/nyccb_index_generator/json_assembler.rb
@@ -6,5 +6,17 @@ require 'json'
 module NyccbIndexGenerator
 
   class JsonAssembler
+
+    attr_reader :section_parts
+
+    def initialize
+      @section_parts = []
+    end
+
+    def get_csv_data(csv_file)
+      CSV.foreach(csv_file, headers: true) do |row|
+        @section_parts << Section.new(row["heading_identifier"], row["catch_text"])
+      end
+    end
   end
 end
